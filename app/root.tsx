957ba790073a42ee051f179494213d713d7a8d77
Elliot <elliotsaaen@hotmail.com>
2024-12-20 09:47:55 +0100

    CSS

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,9 @@
 import { Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node"; // or cloudflare/deno
 
-import "app/tailwind.css";
+import styles from "./tailwind.css?url";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
